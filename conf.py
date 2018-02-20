aa07381cd2e82c1d63d10b64e61987c472c76cfd
Adam J Lemmon <adamjlemmon@gmail.com>
2018-02-20 14:38:22 -0500

    fix tabs in conf

--- conf.py
+++ conf.py
@@ -4,10 +4,10 @@ sys.path.append(os.path.abspath('./course-content'))
 
 def setup(sphinx):
         from SolidityLexer import SolidityLexer
-            sphinx.add_lexer('Solidity', SolidityLexer())
+        sphinx.add_lexer('Solidity', SolidityLexer())
 
-            source_suffix = '.rst'
-            master_doc = 'course-content/index'
-            project = 'Blockchain Learning Group DApp Fundamentals'
-            copyright = '2017, Blockchain Learning Group Inc.'
-            highlight_language = 'Solidity'
+        source_suffix = '.rst'
+        master_doc = 'course-content/index'
+        project = 'Blockchain Learning Group DApp Fundamentals'
+        copyright = '2017, Blockchain Learning Group Inc.'
+        highlight_language = 'Solidity'
