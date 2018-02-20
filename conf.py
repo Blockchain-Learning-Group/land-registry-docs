616b776d7b5dbfd16ff0c59ece6e1272ed268f53
Adam J Lemmon <adamjlemmon@gmail.com>
2018-02-20 14:36:34 -0500

    add conf for rtd sphinx build

---
+++ conf.py
@@ -0,0 +1,13 @@
+import sys, os
+
+sys.path.append(os.path.abspath('./course-content'))
+
+def setup(sphinx):
+        from SolidityLexer import SolidityLexer
+            sphinx.add_lexer('Solidity', SolidityLexer())
+
+            source_suffix = '.rst'
+            master_doc = 'course-content/index'
+            project = 'Blockchain Learning Group DApp Fundamentals'
+            copyright = '2017, Blockchain Learning Group Inc.'
+            highlight_language = 'Solidity'
